3ffc59e2e342a7839deb75aa5081a61bca9ee15b
acubeos <80309463+acubeos@users.noreply.github.com>
2024-04-01 15:51:54 +0100

    chore: removed unauthorized merged code

--- Sheysi/myEmployee.js
+++ Sheysi/myEmployee.js
@@ -1,21 +1,20 @@
 const createEmployee = (name, role, salary) => {
-    return { name, role, salary };
-};
+	return { name, role, salary }
+}
 
-let sasha = createEmployee("Sasha", "Back-end Dev", 2000);
+let Abiola = createEmployee("Sasha", "Back-end Dev", 2000)
 
 const getSalary = (employee) => {
-    return `The salary of ${employee.name} is ${employee.salary} per month`;
-};
+	return `The salary of ${employee.name} is ${employee.salary} per month`
+}
 
-console.log(getSalary(sasha));
-
+console.log(getSalary(sasha))
 
 // Update Salary
 const updateSalary = (employee, newSalary) => {
-    employee.salary = newSalary
-    return `After consideration, ${employee.name}'s new salary is ${employee.salary} per month`;
-};
+	employee.salary = newSalary
+	return `After consideration, ${employee.name}'s new salary is ${employee.salary} per month`
+}
 
-console.log(updateSalary(sasha, 3500));
+console.log(updateSalary(sasha, 3500))
 // console.log(sasha.salary);
