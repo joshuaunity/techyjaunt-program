134c8326ce68bc56d3aec56cd2785af074085e76
acubeos <80309463+acubeos@users.noreply.github.com>
2024-04-01 10:38:54 +0100

    Update the  salary function

--- employee.js
+++ employee.js
@@ -1,11 +1,20 @@
 const createEmployee = (name, role, salary) => {
-    return { name, role, salary };
-};
+	return { name, role, salary }
+}
 
-let micheal = createEmployee("micheal", "frontend dev", 2000);
+let micheal = createEmployee("micheal", "frontend dev", 2000)
 
 const getSalary = (employee) => {
-    return `The salary of ${employee.name} is ${employee.salary} per day`;
-};
+	return `The salary of ${employee.name} is ${employee.salary} per day`
+}
 
-console.log(getSalary(micheal));
+console.log(getSalary(micheal))
+
+// My function to update the salary
+
+const updateSalary = (employee) => {
+	employee.salary = employee.salary + 1000
+	return `The salary of ${employee.name} is now ${employee.salary} per day`
+}
+
+console.log(updateSalary(micheal))
