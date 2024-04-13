df1aa6d89c9dedaafe45e1a93baa206396d8ec4e
Levi ikechukwu <leviikechukwu12@gmail.com>
2024-04-13 15:16:23 -0700

    My first Assignment

---
+++ Levi/index.js
@@ -0,0 +1,46 @@
+var idname = "patrick";
+let age = 34;
+const title = "backend dev";
+
+idname = "chisom";
+age = 48;
+// title = "DevOps";
+
+// console.log(idname);
+// console.log(age);
+// console.log(title);
+
+// Numbers
+let weight = 75; // kg - integer
+let height = 123.5; // cm - float
+
+// strings
+let car = "toyota"
+
+// boolean
+let isTall = true
+let isShort = false
+
+// object
+let bikeOne = { color: "blue", brand: "lexus", speed: 200 }
+// console.log(bikeOne.brand)
+bikeOne.brand = "toyota"
+// console.log(bikeOne.brand)
+let bikeBrands = { date: 1997, bike: bikeOne }
+// console.log(bikeBrands.date)
+// console.log(bikeBrands.bike.brand)
+// console.log(bikeBrands.bike.color)
+
+// Arrays 
+let cars = ["toyota", "honda", "hyundai", "sakura", 6, true]
+// console.log(cars)
+// console.log(cars[2]) // hyundai
+// console.log(cars[4]) // number 6
+console.log(cars.length)
+console.log(cars[cars.length - 1])
+console.log(cars.slice(-1))
+
+
+// some new code - @josh
+console.log("this is a newly added line of code")
+
