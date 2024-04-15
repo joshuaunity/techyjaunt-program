e98deaabaac587ceaccc88731f0ddb29341c59fc
Levi ikechukwu <leviikechukwu12@gmail.com>
2024-04-15 16:10:30 -0700

    modification of updateSalary Function

--- Levi/employee.js
+++ Levi/employee.js
@@ -3,7 +3,7 @@ const createEmployee = (name, role, salary) => {
     return {name, role, salary}
 }
 
-let micheal = createEmployee("micheal", "frontend dev", 2000)
+let micheal = createEmployee("Levi", "frontend dev", 2000)
 
 const getSalary = (employee) => {
     return `The salary of ${employee.name} is ${employee.salary} per day`
@@ -13,10 +13,10 @@ console.log(getSalary(micheal))
 
 //write a function that updates the salary of the employee
 
-const updateSalary = (employee) => {
-    employee.salary = 7000
+const updateSalary = (employee, newSalary) => {
+    employee.salary = newSalary
     return `The new salary of ${employee.name} is now ${employee.salary} per day`
 }
 
-console.log(updateSalary(micheal))
+console.log(updateSalary(micheal, 7000))
 
